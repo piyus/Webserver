53a53294dfc9ff1e99f909274c815ec0f82c9a3d
Piyus <piyus.kedia@outlook.com>
2017-11-13 13:40:45 +0530

    working

--- Enclave/stubs.asm
+++ Enclave/stubs.asm
@@ -5,13 +5,13 @@ EXTERN ocall_generic : PROC
 
 myprintf PROC EXPORT
 mov rax, 0900000000h
-;mov [rax], rcx
-;mov [rax + 8], rdx
-;mov [rax + 16], r8
-;mov [rax + 24], r9
-;mov qword ptr [rax + 32], 1
-;mov r10, [rsp]
-;mov [rax + 40], r10
+mov [rax], rcx
+mov [rax + 8], rdx
+mov [rax + 16], r8
+mov [rax + 24], r9
+mov qword ptr [rax + 32], 1
+mov r10, [rsp]
+mov [rax + 40], r10
 
 mov [rax + 0f8h], rsp
 mov rsp, [rax + 0e8h]
@@ -21,9 +21,9 @@ call ocall_generic
 
 mov rcx, 0900000000h
 mov rsp, [rcx + 0f8h]
-;mov rdx, [rcx + 40]
-;mov [rsp], rdx
-;
+mov rdx, [rcx + 40]
+mov [rsp], rdx
+
 ret
 myprintf ENDP
 
