a0224eacf2929391180255b2142250b8f3a1a7f4
Piyus <piyus.kedia@outlook.com>
2017-11-13 16:27:45 +0530

    try to compile mongoose

--- Enclave/main.asm
+++ Enclave/main.asm
@@ -1,18 +1,14 @@
 .CODE
 
 EXTERN main : PROC
-EXTERN main1 : PROC
 EXTERN ocall_generic : PROC
-
-main2 PROC
-ret
-main2 ENDP
 
 ecall_main PROC EXPORT
 mov rax, 0900000000h
 mov [rax + 0e8h], rsp
 mov rsp, [rax + 0f8h]
 sub rsp, 28h
+mov rcx, 1
 call main
 add rsp, 28h
 mov rcx, 0900000000h
--- Enclave/main.c
+++ Enclave/main.c
@@ -1,17 +1,17 @@
-int  myprintf(const char*, ...);
-int main(){
-	char  name[100];
-	char fmt[] = "%s";
-	scanf(fmt, name);
-	int ret = printf(0x900000100, 123, 456, 789, 1010, name);
-	printf(0x900000100, ret, 456, 789, 1010, name);
-
-	return 0;
-}
-
-
-int main1(){
-
-	//myprintf("Hello world\n");
-	return 0;
-}
+//int  myprintf(const char*, ...);
+//int main(){
+//	char  name[100];
+//	char fmt[] = "%s";
+//	scanf(fmt, name);
+//	int ret = printf(0x900000100, 123, 456, 789, 1010, name);
+//	printf(0x900000100, ret, 456, 789, 1010, name);
+//
+//	return 0;
+//}
+//
+//
+//int main1(){
+//
+//	//myprintf("Hello world\n");
+//	return 0;
+//}
